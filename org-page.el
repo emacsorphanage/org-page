2fb249ee690e2060a3835f73fdc677912ead5683
Kelvin Hu <ini.kelvin@gmail.com>
2012-11-27 11:08:36 +0800

    updated README and description

--- org-page.el
+++ org-page.el
@@ -5,7 +5,7 @@
 ;; Author: Kelvin Hu <ini DOT kelvin AT gmail DOT com>
 ;; Keywords: org-mode, convenience, beautify
 ;; Homepage: https://github.com/kelvinh/org-page
-;; Version: 0.1
+;; Version: 0.2
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -31,7 +31,9 @@
 ;; + customized theme support
 ;; + recent posts support (auto generated)
 ;; + tags support (auto generated)
+;; + categories support (auto generated)
 ;; + search engine support (auto generated)
+;; + commenting (implemented using disqus)
 ;;
 ;; To use org-page, you just need a little configuration,as follows:
 ;;
@@ -41,15 +43,19 @@
 ;;    folder named "src" under this directory, and then org-page will
 ;;    automatically publish them to a folder named "pub", which is also
 ;;    under this directory
-;; 2. `op/publish-search-url': the URL search engine will base on, it
-;;    should be the URL of your personal site
+;; 2. `op/publish-site-url': the URL search engine will base on, and
+;;    also disqus commenting will use this url, it should be the URL of
+;;    your personal site
+;; 3. `op/personal-disqus-shortname': the disqus shortname you own on
+;;    disqus website, will be used for commenting
 ;;
 ;; So, the following simple code will make org-page work:
 ;;
 ;;    (add-to-list 'load-path "path/to/org-page")
 ;;    (require 'org-page)
 ;;    (setq op/root-directory "path/to/your/project")
-;;    (setq op/publish-search-url "your.personal.site.com")
+;;    (setq op/publish-site-url "http://your.personal.site.com/")
+;;    (setq op/personal-disqus-shortname "your_disqus_shortname")
 ;;    (global-set-key (kbd "<f9>") 'op/publish-pages)
 ;;    (global-set-key (kbd "C-<f9>") (lambda () (interactive)
 ;;                                              (op/publish-pages t)))
@@ -59,19 +65,33 @@
 ;; even they have already been published and not modified since last
 ;; publishing, your can just press Ctrl + <F9>.
 ;;
-;; The two variable above are required, and the following variables
+;; The three variables above are required, and the following variables
 ;; are optional, but also could be customized:
 ;;
-;; 1. `op/exclude-filename-regexp': describes which org files in the
-;;    project subdirectory "src" should not be published
-;; 2. `op/include-filename-regexp': describes which org files in the
-;;    project subdirectory "src" should always be published
-;; 3. `op/theme-directory': the directory stores themes, it has a
-;;    default value, you could customize it to your own value
-;; 4. `op/theme': the theme will be used, default value is 'default,
-;;    and sorry for that no more themes are provided at present
-;; 5. `op/tag-directory': the folder stores auto-generated tags
-;; 6. `op/tag-index-filename': filename of auto-generated tag index
+;;  1. `op/exclude-filename-regexp': describes which org files in the
+;;     project subdirectory "src" should not be published
+;;  2. `op/include-filename-regexp': describes which org files in the
+;;     project subdirectory "src" should always be published
+;;  3. `op/theme-directory': the directory stores themes, it has a
+;;     default value, you could customize it to your own value
+;;  4. `op/theme': the theme will be used, default value is 'default,
+;;     and sorry for that no more themes are provided at present
+;;  5. `op/tag-directory': the folder stores auto-generated tags
+;;  6. `op/tag-index-filename': filename of auto-generated tag index
+;;  7. `op/category-directory': the folder stores auto-generated
+;;     categories
+;;  8. `op/category-index-filename': filename of auto-generated tag
+;;     index
+;;  9. `op/publish-site-title': the title of your site
+;; 10. `op/personal-github-link': your personal github link if you
+;;     do have one github account
+;; 11. `op/publish-meta-info': the meta info of current post, it is
+;;     discouraged to change this variable unless you have strong
+;;     knowledge in css and html
+;; 12. `op/publish-comment': the comment section of current post, it
+;;     is also discouraged to change this variable
+;; 13. `op/publish-footer': the footer section of current post, also
+;;     discouraged to change
 
 ;;; Code:
 
