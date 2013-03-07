2e5a44c92f6b4f1b19bab4bd7240f2961393c26a
Kelvin Hu <ini.kelvin@gmail.com>
2013-03-07 16:24:33 +0800

    update export function, add wiki publication support

--- op-export.el
+++ op-export.el
@@ -25,10 +25,12 @@ will return \"this is title\" if OPTION is \"TITLE\""
       (if (re-search-forward match-regexp nil t)
           (match-string-no-properties 2 nil)))))
 
-(defun op/generate-uri (creation-date title)
+(defun op/generate-uri (creation-date title file-type)
   "Generate URI of org file opened in current buffer. It will be firstly read
-from #+URI property, if it is not presented, will be created by CREATION-DATE
-and TITLE, like: /blog/2013/03/07/this-is-post-title.
+from #+URI property, if it is not presented, will be created by CREATION-DATE,
+TITLE and FILE-TYPE, if FILE-TYPE is 'blog, the uri will be like:
+/blog/2013/03/07/this-is-post-title, if FILE-TYPE is 'wiki, the uri will be
+like: /wiki/compare-and-swap
 The #+URI property can be defined containing following parameters:
 %y: year of creation date
 %m: month of creation date
@@ -37,10 +39,24 @@ The #+URI property can be defined containing following parameters:
   (let* ((date-list (split-string creation-date "-"))
          uri)
     (setq uri (or (op/read-org-option "URI")
-                  (concat "/blog/%y/%m/%d/" (convert-string-to-path title))))
+                  (if (eq file-type 'blog)
+                      (concat "/blog/%y/%m/%d/" (convert-string-to-path title))
+                    (concat "/wiki/" (convert-string-to-path title))))) ; TODO customization
     (format-spec uri `((?y . ,(car date-list))
                        (?m . ,(cadr date-list))
                        (?d . ,(caddr date-list))))))
+
+(defun op/get-file-type (org-file)
+  "Get org file type presented by ORG-FILE, will return 'blog or 'wiki.
+How to judge a file is belong to blog or wiki type is based on its root folder
+name under `op/repository-directory'.
+TODO: This function may be improved to have a better type determination system."
+  (let ((full-path (expand-file-name org-file))
+        (wiki-prefix-path (expand-file-name
+                           (concat
+                            (file-name-as-directory op/repository-directory)
+                            "wiki/")))) ; TODO customization
+    (if (string-prefix-p wiki-prefix-path full-path t) 'wiki 'blog)))
 
 (defun op/read-file-info ()
   "Read info of org file opened in current buffer, include:
@@ -50,6 +66,8 @@ The #+URI property can be defined containing following parameters:
 3. tags (read from #+TAGS property)
 4. uri (read from #+URI property)
 5. title
+6. type ('blog or 'wiki, distinguished by their root folder name under
+`op/repository-directory')
 Creation date will be firstly read from #+DATE defined in the file, if no date
 info found, will be read from the file's last change date. However, it is
 recommended to use #+DATE."
@@ -73,9 +91,11 @@ recommended to use #+DATE."
         (plist-put
          attr-plist :tags (delete "" (mapcar 'trim-string
                                              (split-string tags ":" t))))) ;; TODO customization
+    (plist-put attr-plist :type (op/get-file-type filename))
     (plist-put attr-plist :uri (op/generate-uri
                                 (plist-get attr-plist :creation-date)
-                                (plist-get attr-plist :title))))) ; TODO customization
+                                (plist-get attr-plist :title)
+                                (plist-get attr-plist :type))))) ; TODO customization
 
 (defun op/handle-modified-file (org-file-path pub-base-dir)
   "TODO: doc"
