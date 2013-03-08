02977bc788ab0ebcbda6a3a3076ace493fdf1359
Kelvin Hu <ini.kelvin@gmail.com>
2013-03-08 20:41:47 +0800

    add page footer feature

--- op-enhance.el
+++ op-enhance.el
@@ -38,3 +38,39 @@
     (mapconcat '(lambda (css)
                   (format template (concat "/media/css/" css))) ;; TODO customization
                css-list "\n")))
+
+(defun op/generate-footer (uri attr-plist hidden-extra)
+  "Generate page footer, based on the template defined by
+`op/html-postamble-template', please see its description for more detail."
+  (let* ((footer-template op/html-postamble-template)
+         (disqus-identifier uri)
+         (disqus-url (concat (replace-regexp-in-string "/?$" "" op/site-url)
+                             disqus-identifier))
+         (disqus-shortname op/personal-disqus-shortname)
+         (email (confound-email op/email))
+         cdate mdate tags-list tag-links)
+    (when hidden-extra
+      ;; meta info and comment will be hidden
+      (setq footer-template op/footer))
+    (when attr-plist
+      (setq uri (plist-get attr-plist :uri))
+      (setq cdate (plist-get attr-plist :creation-date))
+      (setq mdate (plist-get file-info :mod-date))
+      (setq tag-list (plist-get file-info :tags)))
+    (when tag-list
+      (setq tag-links
+            (mapconcat
+             '(lambda (tag-name)
+                (format-spec "<a href=\"%l\">%n</a>"
+                             `((?l . ,(op/generate-tag-uri tag-name))
+                               (?n . ,tag-name))))
+             tag-list ", ")))
+    (format-spec op/footer-template
+                 `((?a . "%a") (?c . "%c") (?d . "%d") (?e . "%e") (?v . "%v")
+                   (?i . ,(org-html-expand email))
+                   (?h . ,(or cdate "N/A"))
+                   (?m . ,(or mdate "N/A"))
+                   (?t . ,(or tag-links "N/A"))
+                   (?n . ,disqus-identifier)
+                   (?u . ,disqus-url)
+                   (?s . ,disqus-shortname)))))
