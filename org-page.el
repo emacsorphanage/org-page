0c38321710f6c4a6ef407fe6d2be5ac2a5800bd8
Kelvin Hu <ini.kelvin@gmail.com>
2013-08-25 00:28:15 +0800

    update description, for adding repo to melpa

--- org-page.el
+++ org-page.el
@@ -21,7 +21,23 @@
 
 ;;; Commentary:
 
-;; org-page.el contains the main entrance of entire program.
+;; See documentation at https://github.com/kelvinh/org-page
+
+;; Org-page is a static site generator based on org mode.
+
+;; Org-page provides following features:
+
+;; 1) org sources and html files managed by git
+;; 2) incremental publication
+;; 3) category support
+;; 4) tags support (auto generated)
+;; 5) search engine support (auto generated)
+;; 6) a beautiful theme
+;; 7) theme customization support
+;; 8) commenting (implemented using disqus)
+;; 9) site visiting tracking (implemented using google analytics)
+;; 10) index/about page support (auto generated if no default provided)
+;; 11) highly customizable
 
 ;;; Code:
 
